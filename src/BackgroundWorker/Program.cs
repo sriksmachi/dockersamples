c35b4a08389fb8abdb0a13b17c832d7f1c1df606
vishwanath srikanth <srikanthma@live.com>
2015-07-21 06:38:48 +0000

    first push

---
+++ src/BackgroundWorker/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BackgroundWorker
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=sriksstore;AccountKey=KIQ1QGEUveBYibPFMVPMjfEPoQQCE3HCr71yZp/A1YvSuHrMFMK0ZlvqvSrmAym4OA3DwT05suxBMHH3/zDNWQ==");
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queueReference = queueClient.GetQueueReference("messages");
+            while (true)
+            {
+                var message = queueReference.GetMessageAsync().Result;
+                if (message != null)
+                {
+                    var userContext = JsonConvert.DeserializeObject<UserContext>(message.AsString);
+                    //Send Email here
+                    Console.WriteLine(userContext.Email);
+                    queueReference.DeleteMessageAsync(message);
+                }
+                Console.WriteLine("Waiting...");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserContext
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/HelloWorldWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.WindowsAzure;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
+
+namespace HelloWorldWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Submit(string email, string password)
+        {
+            var userContext = new UserContext();
+            userContext.Email = email;
+            userContext.Password = password;
+            var userContextJson = JsonConvert.SerializeObject(userContext);
+            AddToQueue(userContextJson);
+            ViewBag.Message = "Registration Completed Successfully, you will recieve a confirmation mail shortly.";
+            return View("Index");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userContextJson"></param>
+        private async void AddToQueue(string userContextJson)
+        {
+            var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=sriksstore;AccountKey=KIQ1QGEUveBYibPFMVPMjfEPoQQCE3HCr71yZp/A1YvSuHrMFMK0ZlvqvSrmAym4OA3DwT05suxBMHH3/zDNWQ==");
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queueReference = queueClient.GetQueueReference("messages");
+            await queueReference.CreateIfNotExistsAsync();
+            await queueReference.AddMessageAsync(new Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage(userContextJson));
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
